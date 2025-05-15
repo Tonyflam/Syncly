b9d2b2879823a27f3e1b2f132ebea39b6d23f4c4
David Praise <150600391+Tonyflam@users.noreply.github.com>
2025-05-15 10:17:50 +0000

    Update schema.ts

--- src/handler/schema.ts
+++ src/handler/schema.ts
@@ -439,7 +439,7 @@ export default function schema(_: Request, res: Response) {
       {
         name: "FAQ",
         description: "Provides answers to frequently asked questions about ICP.",
-        placeholder: "Choose a FAQ category...",
+        placeholder: "ICPulse is Fecthing FAQ...",
         default_role: "Participant",
         permissions: Permissions.encodePermissions({
           ...emptyPermissions,
