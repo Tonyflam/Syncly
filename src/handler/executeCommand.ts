7634a0852b1121cf0de2e9cfb72112f78f03c2f5
David Praise <150600391+Tonyflam@users.noreply.github.com>
2025-05-15 01:25:16 +0000

    Update executeCommand.ts

--- src/handler/executeCommand.ts
+++ src/handler/executeCommand.ts
@@ -1,19 +1,8 @@
 import { commandNotFound } from "@open-ic/openchat-botclient-ts";
 import { Request, Response } from "express";
 import { withBotClient } from "../types";
-import { handleICPPrice } from "./icp_price";
 import { handleHelp } from "./help";
-import { handleNetworkStatus } from "./network_status";
-import { handleNodeMap } from "./node_map";
-import { handleProposalStats } from "./proposal-stats";
-import { handleProposals } from "./proposals";
-import { handleSNSList } from "./sns-list";
-import { handleSNSProposals } from "./sns-proposals";
-import { handleSubnetVersions } from "./subnet_versions";
-import { handleDailySummary } from "./daily_report";
-import { handleSummarizeProposal } from "./summarize";
-import { handleSubnetLookup } from "./subnet_lookup";
-import { handleNeuronHealthCheck } from "./neuron_health_check";
+import { handleICPPrice } from "./icp_price";
 import { handleFAQ } from "./faq";
 import { handleSNS } from "./sns";
 import { handleGovernance } from "./governance";
@@ -37,11 +26,26 @@ export default async function executeCommand(req: Request, res: Response) {
   console.log("Routing command:", client.commandName);
 
   switch (client.commandName) {
-    case "network":
-      await handleNetwork(req, res);
+    case "icp_price":
+      await handleICPPrice(req, res);
+      break;
+    case "help":
+      await handleHelp(req, res);
+      break;
+    case "daily_report":
+      await handleDailySummary(req, res);
+      break;
+    case "icp_faq":
+      await handleFAQ(req, res);
+      break;
+    case "sns":
+      await handleSNS(req, res);
       break;
     case "governance":
       await handleGovernance(req, res);
+      break;
+    case "network":
+      await handleNetwork(req, res);
       break;
     case "tokens":
       await handleTokens(req, res);
@@ -52,22 +56,14 @@ export default async function executeCommand(req: Request, res: Response) {
     case "dev":
       await handleDev(req, res);
       break;
-    case "sns":
-      await handleSNS(req, res);
-      break;
     case "analytics":
       await handleAnalytics(req, res);
-      break;
-    case "icp_price":
-      await handleICPPrice(req, res);
-      break;
-    case "daily_report":
-      await handleDailySummary(req, res);
-      break;
-    case "help":
-      await handleHelp(req, res);
       break;
     default:
       res.status(400).send(commandNotFound());
   }
+}
+
+function handleDailySummary(req: withBotClient, res: Response<any, Record<string, any>>) {
+  throw new Error("Function not implemented.");
 }
